1402e13fcdd84d75958f9359d7dd58ed6d2a8f3c
Grishin Vadim <43302420+KajimaSoys@users.noreply.github.com>
2024-05-27 01:25:25 +0400

    v.0.3 - code refactoring

--- gui.py
+++ gui.py
@@ -9,7 +9,16 @@ from ant_colony import ACO
 
 
 class TSPGUI:
-    def __init__(self, master):
+    """
+    GUI for solving the Traveling Salesman Problem (TSP) using the Ant Colony Optimization (ACO) algorithm.
+    """
+    def __init__(self, master: tk.Tk) -> None:
+        """
+        Initialize the GUI application.
+
+        Args:
+        master (tk.Tk): The root window object.
+        """
         self.master = master
         master.title("Задача коммивояжёра - оптимизация муравьиной колонией")
 
@@ -18,28 +27,37 @@ class TSPGUI:
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.grid(row=9, column=0, columnspan=6)
 
-        self.points = None
-        self.tsp = None
-        self.aco = None
+        self.points: list[tuple[int, int]] | None = None
+        self.tsp: TSP | None = None
+        self.aco: ACO | None = None
 
         self.setup_widgets()
 
-    def setup_widgets(self):
+    def setup_widgets(self) -> None:
+        """Set up interactive widgets for the GUI."""
+
+        # points_count parameter
         row_start = 0
         ttk.Label(self.master, text="Количество точек (points):").grid(row=row_start, column=0, sticky='se', ipady=4)
         self.points_count = tk.Scale(self.master, from_=1, to=100, orient='horizontal')
         self.points_count.set(20)
         self.points_count.grid(row=row_start, column=1, sticky='we')
+
+        # generate_task button
         ttk.Button(self.master, text="Сгенерировать задачу", command=self.generate_task).grid(row=row_start, column=3, columnspan=1, sticky='swe')
 
+        # ants parameter
         row_start += 1
         ttk.Label(self.master, text="Количество муравьёв (ants):").grid(row=row_start, column=0, sticky='se', ipady=4)
         self.ants_scale = tk.Scale(self.master, from_=1, to=200, orient='horizontal')
         self.ants_scale.set(100)
         self.ants_scale.grid(row=row_start, column=1, sticky='we')
         create_tooltip_icon(self.master, "Общее количество агентов (муравьев), задействованных в одной итерации.", row_start, 2)
+
+        # perform_calculation button
         ttk.Button(self.master, text="Расчёт", command=self.perform_calculation).grid(row=row_start, column=3, columnspan=1, sticky='swe')
 
+        # iter parameter
         row_start += 1
         ttk.Label(self.master, text="Количество итераций (iter):").grid(row=row_start, column=0, sticky='se', ipady=4)
         self.iter_scale = tk.Scale(self.master, from_=1, to=100, orient='horizontal')
@@ -47,6 +65,7 @@ class TSPGUI:
         self.iter_scale.grid(row=row_start, column=1, sticky='we')
         create_tooltip_icon(self.master, "Максимальное количество итераций алгоритма.", row_start, 2)
 
+        # alpha parameter
         row_start += 1
         ttk.Label(self.master, text="Альфа (α):").grid(row=row_start, column=0, sticky='se', ipady=4)
         self.alpha_scale = tk.Scale(self.master, from_=0.0, to=3.0, orient='horizontal', resolution=0.1)
@@ -54,6 +73,7 @@ class TSPGUI:
         self.alpha_scale.grid(row=row_start, column=1, sticky='we')
         create_tooltip_icon(self.master, "Коэффициент, контролирующий влияние феромона на ребре.", row_start, 2)
 
+        # beta parameter
         row_start += 1
         ttk.Label(self.master, text="Бета (β):").grid(row=row_start, column=0, sticky='se', ipady=4)
         self.beta_scale = tk.Scale(self.master, from_=0.0, to=3.0, orient='horizontal', resolution=0.1)
@@ -61,6 +81,7 @@ class TSPGUI:
         self.beta_scale.grid(row=row_start, column=1, sticky='we')
         create_tooltip_icon(self.master, "Коэффициент, контролирующий влияние привлекательности маршрута.", row_start, 2)
 
+        # p(Ro) parameter
         row_start += 1
         ttk.Label(self.master, text="Коэффициент испарения феромона (ρ):").grid(row=row_start, column=0, sticky='se', ipady=4)
         self.p_scale = tk.Scale(self.master, from_=0.0, to=1.0, orient='horizontal', resolution=0.1)
@@ -68,6 +89,7 @@ class TSPGUI:
         self.p_scale.grid(row=row_start, column=1, sticky='we')
         create_tooltip_icon(self.master, "Отражает степень взаимного влияния между муравьями, предотвращает бесконечное накопление феромона.", row_start, 2)
 
+        # q parameter
         row_start += 1
         ttk.Label(self.master, text="Интенсивность феромона (q):").grid(row=row_start, column=0, sticky='se', ipady=4)
         self.q_scale = tk.Scale(self.master, from_=1, to=100, orient='horizontal')
@@ -75,12 +97,14 @@ class TSPGUI:
         self.q_scale.grid(row=row_start, column=1, sticky='we')
         create_tooltip_icon(self.master, "Общее количество феромонов, влияющее на скорость сходимости алгоритма.", row_start, 2)
 
-    def generate_task(self):
+    def generate_task(self) -> None:
+        """Generate a new TSP problem."""
         self.points = generate_problem(int(self.points_count.get()))
         self.tsp = TSP(self.points)
         self.update_plot()
 
-    def perform_calculation(self, event=None):
+    def perform_calculation(self, event: tk.Event | None = None) -> None:
+        """Perform the ACO calculation and update the plot."""
         if self.points:
             ants = int(self.ants_scale.get())
             iterations = int(self.iter_scale.get())
@@ -94,7 +118,8 @@ class TSPGUI:
             self.tsp._paths = [Path(indx=best_path.indx, leng=best_path.leng, name="ACO Path")]
             self.update_plot()
 
-    def update_plot(self):
+    def update_plot(self) -> None:
+        """Update the matplotlib plot with the current TSP solution."""
         if self.tsp:
             self.ax.clear()
             self.tsp.show(self.ax)
--- utils/tooltip.py
+++ utils/tooltip.py
@@ -2,43 +2,53 @@ import tkinter as tk
 from tkinter import ttk
 
 
-def create_tooltip_icon(master, text, row, column):
-    icon = ttk.Label(master, text='?', font=('Arial', 12, 'bold'), foreground='black')
-    icon.grid(row=row, column=column, sticky='sw')
-    ToolTip(icon, text=text)
-
-
 class ToolTip(object):
-    def __init__(self, widget, text='widget info'):
+    """
+    Class to create and manage tooltips for Tkinter widgets.
+    """
+    def __init__(self, widget: tk.Widget, text: str = 'widget info') -> None:
+        """
+        Initialize the tooltip with a widget.
+
+        Args:
+        widget (tk.Widget): The widget to which the tooltip is attached.
+        text (str): The text displayed by the tooltip.
+        """
         self.widget = widget
         self.text = text
-        self.tipwindow = None
-        self.id = None
+        self.tipwindow: tk.Toplevel | None = None
+        self.id: str | None = None
         self.x = self.y = 0
         self.create_tooltip()
 
-    def create_tooltip(self):
+    def create_tooltip(self) -> None:
+        """Bind mouse events for showing and hiding the tooltip."""
         self.widget.bind("<Enter>", self.enter)
         self.widget.bind("<Leave>", self.leave)
 
-    def enter(self, event=None):
+    def enter(self, event: tk.Event | None = None) -> None:
+        """Schedule the tooltip to be shown after a delay when the mouse enters the widget."""
         self.schedule()
 
-    def leave(self, event=None):
+    def leave(self, event: tk.Event | None = None) -> None:
+        """Unschedule and hide the tooltip when the mouse leaves the widget."""
         self.unschedule()
         self.hide_tip()
 
-    def schedule(self):
+    def schedule(self) -> None:
+        """Schedule the tooltip to appear after a delay."""
         self.unschedule()
-        self.id = self.widget.after(500, self.show_tip)  # 500ms до показа подсказки
+        self.id = self.widget.after(500, self.show_tip)  # Delay in milliseconds
 
-    def unschedule(self):
+    def unschedule(self) -> None:
+        """Cancel the scheduled appearance of the tooltip if any."""
         id = self.id
         self.id = None
         if id:
             self.widget.after_cancel(id)
 
-    def show_tip(self, event=None):
+    def show_tip(self, event: tk.Event | None = None) -> None:
+        """Display the tooltip near the widget."""
         x = y = 0
         x, y, cx, cy = self.widget.bbox("insert")
         x += self.widget.winfo_rootx() + 25
@@ -47,13 +57,27 @@ class ToolTip(object):
         self.tipwindow = tw = tk.Toplevel(self.widget)
         tw.wm_overrideredirect(True)
         tw.wm_geometry("+%d+%d" % (x, y))
-        label = tk.Label(tw, text=self.text, justify=tk.LEFT,
-                         background="#ffffe0", relief=tk.SOLID, borderwidth=1,
-                         font=("tahoma", "8", "normal"))
+        label = tk.Label(tw, text=self.text, justify=tk.LEFT, background="#ffffe0", relief=tk.SOLID, borderwidth=1, font=("tahoma", "8", "normal"))
         label.pack(ipadx=1)
 
-    def hide_tip(self):
+    def hide_tip(self) -> None:
+        """Destroy the tooltip window."""
         tw = self.tipwindow
         self.tipwindow = None
         if tw:
             tw.destroy()
+
+
+def create_tooltip_icon(master: tk.Widget, text: str, row: int, column: int) -> None:
+    """
+    Create a tooltip icon (a label with a '?') that shows a tooltip when hovered.
+
+    Args:
+    master (tk.Widget): The master Tkinter widget.
+    text (str): The tooltip text.
+    row (int): The grid row to place the tooltip icon.
+    column (int): The grid column to place the tooltip icon.
+    """
+    icon = ttk.Label(master, text='?', font=('Arial', 12, 'bold'), foreground='black')
+    icon.grid(row=row, column=column, sticky='sw')
+    ToolTip(icon, text=text)
